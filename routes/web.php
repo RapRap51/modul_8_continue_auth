e51002d8d37c9c604f964863ed8b998863b0e5b1
RapRap <jembercity.rafi0051@gmail.com>
2023-06-26 22:30:48 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::get('home', [HomeController::class, 'index'])->name('home');
 Route::get('profile', ProfileController::class)->name('profile');
 
 Route::resource('employees', EmployeeController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
